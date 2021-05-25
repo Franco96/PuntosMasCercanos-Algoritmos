a3a3f14eec61f510b502a3bd698a9e9b60d9623a
Franco96 <francolautarocarranza@hotmail.com>
2021-05-25 18:30:37 -0300

    Subiendo algoritmo basico

---
+++ src/Simple.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+public class Simple {
+
+	public static void main(String[] args) {
+	
+		
+		//Tienen que ser positivos los puntos [0,10^9]
+		
+		
+
+	
+		ArrayList<Punto> listaPuntos = new ArrayList<Punto>( );
+		
+	
+		listaPuntos.add(new Punto(1, 1));
+		listaPuntos.add(new Punto(2, 2));
+		listaPuntos.add(new Punto(1, 0));
+		listaPuntos.add(new Punto(222, 3));
+		listaPuntos.add(new Punto(44410, 3));
+		listaPuntos.add(new Punto(14440, 3));
+	
+			
+		Punto [] puntosMasCercanos = algoritmoBasico(listaPuntos);
+		
+		System.out.println(puntosMasCercanos[0].toString()+"-"+puntosMasCercanos[1].toString());
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	public static Punto[] algoritmoBasico(List<Punto> listaPuntos ){
+		
+		Punto [] puntosMasCercanos = {new Punto(0, 0),new Punto(0, 0)}; 
+	
+		
+		double distToReturn = Double.MAX_VALUE;
+		
+		for(int i= 0;i<listaPuntos.size();i++){
+				
+			for (int j=i+1; j<listaPuntos.size();j++){
+					
+				double distanciaActual = distancia(listaPuntos.get(i),listaPuntos.get(j));
+				
+				
+				if(distToReturn>distanciaActual){
+					
+					distToReturn = distanciaActual;
+					puntosMasCercanos[0] = listaPuntos.get(i);
+					puntosMasCercanos[1] = listaPuntos.get(j);
+				}
+				
+				
+			}
+			
+		}
+		
+		
+		return puntosMasCercanos;
+			
+	}
+	
+	
+	public static double distancia(Punto p1,Punto p2) {
+		long dx = (p1.getX() - p2.getX()); 
+		long dy = (p1.getY() - p2.getY());
+		return Math.sqrt(dx*dx + dy*dy); 
+		
+	}
+
+}
